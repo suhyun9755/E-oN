2b83f15dfb3e05219dbd8403396d43633f63bf08
suhyun9755 <50443795+suhyun9755@users.noreply.github.com>
2019-05-30 20:03:29 +0900

    Add files via upload

--- Project7/Project7/소스.c
+++ Project7/Project7/소스.c
@@ -1,37 +1,37 @@
-/*다이아몬드
-중간 부분의 *의 개수(홀수)를 입력받고
-다이아몬드가 출력되는 프로그램을 구성하시오.
-*/
+/*두 사각형 좌표가 주어질 때 두 사각형이 오버랩(overlap) 되는지 아닌지를 판단하는 프로그램을 작성하시오.
+변을 공유하거나 꼭지점을 공유하는 경우에도 overlap 되는 것으로 간주한다.
+연산과정에 포인터 변수를 섞어서 진행할 것.*/
+
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-int main()
+#include<stdio.h>
+
+void square(int *x1, int *x2, int *y1, int *y2, int *x3, int *x4, int *y3, int *y4)
 {
-	int iValue, istar, iblank;
-
-	printf("중간부분의 홀수 값을 입력하시오 : ");
-	scanf("%d", &iValue);
-
-	for (istar = 1; istar <= iValue; istar++) {
-		if (istar % 2 == 0)
-			continue;
-		for (iblank = 0; iblank < ((iValue - istar) / 2); iblank++)
-			printf(" ");
-		for (iblank = 0; iblank < istar; iblank++) {
-			printf("*");
-		}
-		printf("\n");
+	if (((*x1 < *x3) && (*x2 < *x3)) || ((*x1 > *x4) && (*x2 > *x4)))
+	{
+		printf("No overlap");
 	}
-	for (istar = (iValue - 1); istar >= 1; istar--) {
-		if (istar % 2 == 0)
-			continue;
-		for (iblank = 0; iblank < ((iValue - istar) / 2); iblank++)
-			printf(" ");
-
-		for (iblank = 0; iblank < istar; iblank++) {
-			printf("*");
-		}
-		printf("\n");
+	else if (((*y1 < *y3) && (*y2 < *y3)) || ((*y1 > *y4) && (*y2 > *y4)))
+	{
+		printf("No overlap");
 	}
+	else {
+		printf("overlap");
+	}
+
+}
+int main(void)
+{
+	int x1, y1, x2, y2;
+	int x3, y3, x4, y4;
+
+	printf("1좌표를 입력 하시오 : ");
+	scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+	printf("2좌표를 입력 하시오 : ");
+	scanf("%d %d %d %d", &x3, &y3, &x4, &y4);
+
+	square(&x1, &x2, &y1, &y2, &x3, &x4, &y3, &y4);
+
 
 	return 0;
 }
